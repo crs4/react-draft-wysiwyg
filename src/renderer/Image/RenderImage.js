11872e9bfc08cf3ce1f6e5baec80daacb5e2343b
Stefano Leone Monni <stefano.monni@crs4.it>
2022-07-07 19:35:11 +0200

    added RenderImage size

--- src/renderer/Image/RenderImage.js
+++ src/renderer/Image/RenderImage.js
@@ -1,32 +1,42 @@
 import React from 'react';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
+import reactImageSize from 'react-image-size';
 
 export const RenderImage = (props) => {
 
-    const [imgWidth,setimgWidth] = useState(0)
-    const [imgHeight,setimgHeight] = useState(0)
-    const maxDim = Math.min(window.innerWidth, window.innerHeight)*0.5;
-    
-    useEffect(()=>{
+    const [imgWidth, setimgWidth] = useState(0)
+    const [imgHeight, setimgHeight] = useState(0)
+    const maxDimX = window.innerWidth * 0.5;
+    const maxDimY = window.innerHeight * 0.5;
+
+    useEffect(() => {
         const { width, height, src } = props;
-          if (width=="auto" && height=="auto")
-          {
-            setimgWidth(maxDim);
-            return;
-          }
-          if (width>height)
-            setimgWidth(width)
-            else setimgHeight(height);
+
+        const getImageSize = async () => {
+            if (width == "auto" && height == "auto") {
+                const { width, height } = await reactImageSize(src);
+                if (width > height)
+                    setimgWidth(width);
+                else setimgHeight(height);
+            }
+            else
+            {
+                if (width!="auto") setimgWidth(width);
+                if (height!="auto") setimgHeight(height);
+            }
+
             console.log(`RENDER IMAGE (LIB): width:${width} height:${height} src:${src}`);
         }
 
-        
-    ,[])
+        getImageSize();
+
+    }
+    , [])
 
 
-   return ((imgWidth>0) ? 
-        <img alt={props.alt} style={{padding:"10ox"}} src={props.src} width={`${Math.min(imgWidth,maxDim)}px`}></img>
+    return ((imgWidth > 0) ?
+        <img alt={props.alt} style={{ padding: "10ox" }} src={props.src} width={`${Math.min(imgWidth, maxDimX)}px`}></img>
         :
-        <img alt={props.alt}  style={{padding:"10ox"}} src={props.src} height={`${Math.min(imgHeight,maxDim)}px`}></img>
+        <img alt={props.alt} style={{ padding: "10ox" }} src={props.src} height={`${Math.min(imgHeight, maxDimY)}px`}></img>
     )
 }

cc4ce3f882a4f9d5742831e308be31621ba2413f
Stefano Leone Monni <stefano.monni@crs4.it>
2022-07-11 19:10:50 +0200

    RenderImage improvements

--- src/renderer/Image/RenderImage.js
+++ src/renderer/Image/RenderImage.js
@@ -3,19 +3,21 @@ import { useEffect, useState } from 'react';
 import reactImageSize from 'react-image-size';
 
 
-
+//@audit RenderImage
 export const RenderImage = (props) => {
 
     const [imgWidth, setimgWidth] = useState(0)
     const [imgHeight, setimgHeight] = useState(0)
-    const maxDimX = window.innerWidth * 0.5;
-    const maxDimY = window.innerHeight * 0.5;
+    const xscalefactor = props.xscalefactor ||  0.5
+    const yscalefactor = props.yscalefactor || 0.5
+    const maxDimX = window.innerWidth*xscalefactor
+    const maxDimY = window.innerHeight*yscalefactor
 
     useEffect(() => {
 
         const { width, height, src } = props;
 
-        if (width == "auto" && height == "auto") {
+        if (width == "auto" && height == "auto" || (width==null && height==null)) {
             reactImageSize(src).then(({ width, height }) => {
                 {
                     if (width > height)
@@ -33,8 +35,8 @@ export const RenderImage = (props) => {
 
 
     return ((imgWidth > 0) ?
-        <img alt={props.alt} style={{ padding: "10ox" }} src={props.src} width={`${Math.min(imgWidth, maxDimX)}px`}></img>
+        <img alt={props.alt} style={{ padding: "10px" }} src={props.src} width={`${Math.min(imgWidth, maxDimX)}px`}></img>
         :
-        <img alt={props.alt} style={{ padding: "10ox" }} src={props.src} height={`${Math.min(imgHeight, maxDimY)}px`}></img>
+        <img alt={props.alt} style={{ padding: "10px" }} src={props.src} height={`${Math.min(imgHeight, maxDimY)}px`}></img>
     )
 }
